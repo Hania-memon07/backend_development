e20122f752e0bec8cacb3e42899ccf8e79de80ca
hania-memon07 <shahzadmrs79@gmail.com>
2025-06-12 15:48:45 +0500

    Initial commit

---
+++ first.js
@@ -0,0 +1,51 @@
+// variables:
+// container which stores data
+// let,var,const(declaration)
+// let variablename=variabledata(syntax)
+let name="hania";//string
+let age=25;//number
+let agree=true;//boolean
+
+let empty=null //null empty
+//onsole.log(name) //for printing
+
+//string methods:
+ console.log(name.toUpperCase())
+console.log(name2.length)
+console.log(name2.toUpperCase())
+console.log(name2.toLowerCase())
+
+// datatypes
+//arrays
+let colors=['red','green']
+ console.log(colors)
+// .push(6) will add 6 
+//.pop()     will remove last one     
+//.length calculate length  
+//num start from 1 index with 0
+
+
+
+
+//this is my comments
+
+
+
+if(condition){  
+ saatements}
+ else {
+
+ }
+
+ function functionname(parameter){ 
+    return //will not print
+   }
+
+let variable=functionname(arguments)
+
+// arrow fctn: const variablename=(parameters)=>{function}
+
+for(let i=0;i<=5; i++){
+
+}
+// let count=0 while(count<){statemebts count++}
